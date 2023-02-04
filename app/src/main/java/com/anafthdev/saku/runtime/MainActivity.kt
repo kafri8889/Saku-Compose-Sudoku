0f17b5e5fca12e95017e89202af016da619e6d4f
kafri8889 <kafri8889@gmail.com>
2023-02-04 15:28:41 +0700

    add `AndroidEntryPoint` annotation to MainActivity

--- app/src/main/java/com/anafthdev/saku/runtime/MainActivity.kt
+++ app/src/main/java/com/anafthdev/saku/runtime/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.anafthdev.saku.runtime.navigation.SakuNavigation
 import com.anafthdev.saku.theme.SakuTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
