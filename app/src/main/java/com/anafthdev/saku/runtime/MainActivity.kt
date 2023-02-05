6cdc6a9a30d8e6b3a99536df76637a8620d0de82
kafri8889 <kafri8889@gmail.com>
2023-02-05 20:39:58 +0700

    setDecorFitsSystemWindows to false

--- app/src/main/java/com/anafthdev/saku/runtime/MainActivity.kt
+++ app/src/main/java/com/anafthdev/saku/runtime/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import com.anafthdev.saku.runtime.navigation.SakuNavigation
 import com.anafthdev.saku.theme.SakuTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -15,6 +16,9 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
+		
+		WindowCompat.setDecorFitsSystemWindows(window, false)
+		
 		setContent {
 			SakuTheme {
 				Surface(
