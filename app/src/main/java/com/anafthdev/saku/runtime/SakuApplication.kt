ed26075f6ce02b874f91a10173414ab17cbf98c4
kafri8889 <kafri8889@gmail.com>
2023-02-12 08:46:09 +0700

    Plant timber

--- app/src/main/java/com/anafthdev/saku/runtime/SakuApplication.kt
+++ app/src/main/java/com/anafthdev/saku/runtime/SakuApplication.kt
@@ -1,8 +1,15 @@
 package com.anafthdev.saku.runtime
 
 import android.app.Application
+import com.anafthdev.saku.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class SakuApplication: Application() {
+	
+	override fun onCreate() {
+		super.onCreate()
+		if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+	}
 }
