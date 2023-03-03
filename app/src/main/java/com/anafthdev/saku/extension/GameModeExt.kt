060ad4731597daedb68d690f44e9f52d10368050
kafri8889 <kafri8889@gmail.com>
2023-03-03 11:38:00 +0700

    Change game mode missing digits

--- app/src/main/java/com/anafthdev/saku/extension/GameModeExt.kt
+++ app/src/main/java/com/anafthdev/saku/extension/GameModeExt.kt
@@ -4,9 +4,9 @@ import com.anafthdev.saku.data.GameMode
 
 val GameMode.missingDigits: Int
 	get() = when (this) {
-		GameMode.Fast -> 28
-		GameMode.Easy -> 36
-		GameMode.Normal -> 44
-		GameMode.Hard -> 52
-		GameMode.Evil -> 60
+		GameMode.Fast -> 24
+		GameMode.Easy -> 32
+		GameMode.Normal -> 40
+		GameMode.Hard -> 48
+		GameMode.Evil -> 56
 	}
