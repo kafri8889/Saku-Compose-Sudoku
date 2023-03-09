f43a5acffed9300947b9347cc485be42663faec0
kafri8889 <kafri8889@gmail.com>
2023-03-09 20:33:30 +0700

    Add app icon

--- app/src/main/java/com/anafthdev/saku/extension/DifficultyExt.kt
+++ app/src/main/java/com/anafthdev/saku/extension/DifficultyExt.kt
@@ -5,7 +5,7 @@ import com.anafthdev.saku.data.Difficulty
 
 val Difficulty.missingDigits: Int
 	get() = when (this) {
-		Difficulty.Fast -> 24
+		Difficulty.Fast -> 1
 		Difficulty.Easy -> 32
 		Difficulty.Normal -> 40
 		Difficulty.Hard -> 48
