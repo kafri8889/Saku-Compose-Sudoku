65bea64f6446c4505081b7321696c903630d8ddc
kafri8889 <kafri8889@gmail.com>
2023-03-11 14:43:31 +0700

    Add `navigationBarsPadding`

--- app/src/main/java/com/anafthdev/saku/ui/print/PrintScreen.kt
+++ app/src/main/java/com/anafthdev/saku/ui/print/PrintScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
@@ -65,6 +66,7 @@ fun PrintScreen(
 	Column(
 		modifier = Modifier
 			.padding(8.dp)
+			.navigationBarsPadding()
 	) {
 		Row(
 			verticalAlignment = Alignment.CenterVertically,
